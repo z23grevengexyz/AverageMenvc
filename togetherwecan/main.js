51e5c4bbade0a458286c1074fca735ab3eef02fa
Ruddy Setiadi Gunawan <dary1039gbz@outlook.com>
2022-01-12 14:58:31 -0500

    update

--- togetherwecan/main.js
+++ togetherwecan/main.js
@@ -1,27 +1,26 @@
-/* Moralis init code */
+
 const serverUrl = "https://bzickdhawq4z.usemoralis.com:2053/server";
 const appId = "qITe7GuLdt3ciuexNB8FW94lY3I08GlzPJeE8Hyk";
 Moralis.start({ serverUrl, appId });
 
-/* Authentication code */
+
 async function login() {
-    let user = Moralis.User.current();
-    if (!user) {
-      user = await Moralis.authenticate({ signingMessage: "Log in using Moralis" })
-        .then(function (user) {
-          console.log("logged in user:", user);
-          console.log(user.get("ethAddress"));
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    }
+  let user = Moralis.User.current();
+  if (!user) {
+   try {
+      user = await Moralis.authenticate({ signingMessage: "Hello World!" })
+      console.log(user)
+      console.log(user.get('ethAddress'))
+   } catch(error) {
+     console.log(error)
+   }
   }
-  
-  async function logOut() {
-    await Moralis.User.logOut();
-    console.log("logged out");
-  }
-  
-  document.getElementById("btn-login").onclick = login;
-  document.getElementById("btn-logout").onclick = logOut;
+}
+
+async function logOut() {
+  await Moralis.User.logOut();
+  console.log("logged out");
+}
+
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
