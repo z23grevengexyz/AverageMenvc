a9dce1867cd815b32fe96c3842f11cef97aa5610
Ruddy Setiadi Gunawan <dary1039gbz@outlook.com>
2022-01-12 14:53:29 -0500

    update main.js

--- togetherwecan/main.js
+++ togetherwecan/main.js
@@ -1,1 +1,6 @@
-/* TODO: Add Moralis init code */
+/* Moralis init code */
+const serverUrl = "https://bzickdhawq4z.usemoralis.com:2053/server";
+const appId = "qITe7GuLdt3ciuexNB8FW94lY3I08GlzPJeE8Hyk";
+Moralis.start({ serverUrl, appId });
+
+/* TODO: Add Moralis Authentication code */
