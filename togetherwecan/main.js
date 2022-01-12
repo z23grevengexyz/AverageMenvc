78e7090b51e0efce97de0faa3e6c6af640736c1e
Ruddy Setiadi Gunawan <dary1039gbz@outlook.com>
2022-01-12 16:05:01 -0500

    update

---
+++ togetherwecan/main.js
@@ -0,0 +1,27 @@
+/* Moralis init code */
+const serverUrl = "https://bzickdhawq4z.usemoralis.com:2053/server";
+const appId = "qITe7GuLdt3ciuexNB8FW94lY3I08GlzPJeE8Hyk";
+Moralis.start({ serverUrl, appId });
+
+/* Authentication code */
+async function login() {
+  let user = Moralis.User.current();
+  if (!user) {
+    user = await Moralis.authenticate({ signingMessage: "Welcome to AverageMen" })
+      .then(function (user) {
+        console.log("logged in user:", user);
+        console.log(user.get("ethAddress"));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+}
+
+async function logOut() {
+  await Moralis.User.logOut();
+  console.log("logged out");
+}
+
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
