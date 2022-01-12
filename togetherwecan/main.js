8914cfcf7b3af99f347d306ed509523bdfc58e61
Ruddy Setiadi Gunawan <dary1039gbz@outlook.com>
2022-01-12 14:54:08 -0500

    update main.js

--- togetherwecan/main.js
+++ togetherwecan/main.js
@@ -3,4 +3,25 @@ const serverUrl = "https://bzickdhawq4z.usemoralis.com:2053/server";
 const appId = "qITe7GuLdt3ciuexNB8FW94lY3I08GlzPJeE8Hyk";
 Moralis.start({ serverUrl, appId });
 
-/* TODO: Add Moralis Authentication code */
+/* Authentication code */
+async function login() {
+    let user = Moralis.User.current();
+    if (!user) {
+      user = await Moralis.authenticate({ signingMessage: "Log in using Moralis" })
+        .then(function (user) {
+          console.log("logged in user:", user);
+          console.log(user.get("ethAddress"));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
+  
+  async function logOut() {
+    await Moralis.User.logOut();
+    console.log("logged out");
+  }
+  
+  document.getElementById("btn-login").onclick = login;
+  document.getElementById("btn-logout").onclick = logOut;
