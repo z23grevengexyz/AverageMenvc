c5b284d6e69de81ce24994bc5e7dc53ad1a2ea43
Ruddy Setiadi Gunawan <dary1039gbz@outlook.com>
2022-01-12 16:54:50 -0500

    update

--- togetherwecan/main.js
+++ togetherwecan/main.js
@@ -10,7 +10,7 @@ async function login() {
   let user = Moralis.User.current();
   if (!user) {
     //user = await Moralis.authenticate({ signingMessage: "Welcome to AverageMen" })
-    user = await Moralis.enableWeb3 ();
+    user = await Moralis.enableWeb3 ({chainId: 56});
       .then(function (user) {
         console.log("logged in user:", user);
         console.log(user.get("ethAddress"));
